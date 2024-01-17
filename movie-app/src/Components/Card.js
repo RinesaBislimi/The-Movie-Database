5fdbb035dc73090e24686930392dddcbcbed9d64
EndritReqica1 <130557822+EndritReqica1@users.noreply.github.com>
2024-01-17 11:28:16 +0100

    test10

--- movie-app/src/Components/Card.js
+++ movie-app/src/Components/Card.js
@@ -19,11 +19,16 @@ const Card = ({ info }) => {
           <div className="modal">
             <div className="modal-header">
               <h3>{info.title}</h3>
-              <button onClick={handleModalClose}>&times;</button>
+              <button className="titulli_x" onClick={handleModalClose}>&times;</button>
             </div>
-            <div className="modal-body">
-              <img src={`https://image.tmdb.org/t/p/w500${info.poster_path}`} alt={info.title} className="img-fluid" />
-              <p>{info.overview}</p>
+            <div className="modal-body secondclass">
+                <div className="modal_body_img">
+                <img src={`https://image.tmdb.org/t/p/w500${info.poster_path}`} alt={info.title} className="img-fluid" />
+                </div>
+                <div className="modal_body_p">
+                <p>{info.overview}</p>
+                </div>
+            
               {/* Add other details you want to display in the modal */}
             </div>
           </div>
