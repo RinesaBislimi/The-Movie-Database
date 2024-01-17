ed104082c409915e7d5e5bee324f945e61934bac
EndritReqica1 <130557822+EndritReqica1@users.noreply.github.com>
2024-01-17 11:00:28 +0100

    test6

--- movie-app/src/Components/Card.js
+++ movie-app/src/Components/Card.js
@@ -1,23 +1,35 @@
-import react from 'react';
-
-const Card =(movie)=>{
-    console.log(movie.info);
-    let img_path="https://image.tmdb.org/t/p/w500";
-    return(
-        <>
-        <div className="movie">
-            <img src={img_path+movie.info.poster_path} className="poster"></img>
-            <div className="movie-details">
-                    <div className='box'>
-                        <h4 className='title'>{movie.info.title}</h4>
-                        <p className='rating'>{movie.info.vote_average}</p>
-                    </div>
-            </div>
+import React, { useState } from "react";
+const Card = ({ info }) => {
+  const [showModal, setShowModal] = useState(false);
+  const handleModalClose = () => setShowModal(false);
+  const handleModalShow = () => setShowModal(true);
+  return (
+    <>
+      <div className="movie" onClick={handleModalShow} style={{ cursor: "pointer" }}>
+        <img src={`https://image.tmdb.org/t/p/w500${info.poster_path}`} className="poster" alt={info.title} />
+        <div className="movie-details">
+          <div className="box">
+            <h4 className="title">{info.title}</h4>
+            <p className="rating">{info.vote_average}</p>
+          </div>
         </div>
-        </>
-
-
-    )
-}
-
-export default Card
+      </div>
+      {showModal && (
+        <div className="modal-overlay" onClick={handleModalClose}>
+          <div className="modal">
+            <div className="modal-header">
+              <h3>{info.title}</h3>
+              <button onClick={handleModalClose}>&times;</button>
+            </div>
+            <div className="modal-body">
+              <img src={`https://image.tmdb.org/t/p/w500${info.poster_path}`} alt={info.title} className="img-fluid" />
+              <p>{info.overview}</p>
+              {/* Add other details you want to display in the modal */}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default Card;
---
+++ movie-app/src/Components/movieDetail/movie.js
@@ -0,0 +1,3 @@
+import React, {useEffect, useState} from "react";
+import "./movie.css";
+import { useParams } from "react-router-dom"
